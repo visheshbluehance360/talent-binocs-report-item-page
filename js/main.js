2c1d20d7097d57db70bb1173763863c0f58251bc
Vishesh <vishesh.bluehance@gmail.com>
2025-03-27 14:07:58 +0530

    Refactor data loading logic; restore fetch functionality and update body class handling

--- js/main.js
+++ js/main.js
@@ -12,39 +12,41 @@ addEventListener("load", () => {
 });
 
 function getPageData() {
-  populateData(sampleData);
-
-  const body = document.querySelector("body");
-  body.classList.remove("loading-data");
-
-  return "test";
-
-  // const siteMainUrl = "https://rprochow.wixsite.com/themindsetiq";
-  // const httpFunctionName = "reportpagecontent";
-  // const pageItemId = "e005e670-abd8-4555-a8b4-ff8f0e6f07c9";
-
-  // const url = `${siteMainUrl}/_functions/${httpFunctionName}/${pageItemId}`;
+  /* --------- test --------- */
+  // populateData(sampleData);
 
   // const body = document.querySelector("body");
+  // body.classList.remove("loading-data");
 
-  // fetch(url)
-  //   .then(res => {
-  //     if (res.headers.get('content-type')?.includes('application/json')) {
-  //       return res.json();
-  //     }
-  //     return res.text().then(text => { throw new Error(text); });
-  //   })
-  //   .then(data => {
-  //     if (!data || data.error) {
-  //       body.classList.add("report-not-found");
-  //     } else {
-  //       populateData(data);
-  //     }
-  //   })
-  //   .catch(console.error)
-  //   .finally(() => {
-  //     body.classList.remove("loading-data");
-  //   });
+  // return "test";
+
+  /* ------ prod ------ */
+  const siteMainUrl = "https://rprochow.wixsite.com/themindsetiq";
+  const httpFunctionName = "reportpagecontent";
+  const pageItemId = "e005e670-abd8-4555-a8b4-ff8f0e6f07c9";
+
+  const url = `${siteMainUrl}/_functions/${httpFunctionName}/${pageItemId}`;
+
+  const body = document.querySelector("body");
+
+  fetch(url)
+    .then(res => {
+      if (res.headers.get('content-type')?.includes('application/json')) {
+        return res.json();
+      }
+      return res.text().then(text => { throw new Error(text); });
+    })
+    .then(data => {
+      if (!data || data.error) {
+        body.classList.add("report-not-found");
+      } else {
+        populateData(data);
+      }
+    })
+    .catch(console.error)
+    .finally(() => {
+      body.classList.remove("loading-data");
+    });
 }
 
 const sampleData = {
